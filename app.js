6ff29bbe954b6fa6d161fad33fb251157cd181dd
David Inga <david.inga@icloud.com>
2013-04-29 21:24:34 +0200

    revision de pull request

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
 // Cargamos módulos Node
 var express = require('express');
 var http = require('http');
+var passport = require('passport');
 
 // Creamos aplicación
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
+
+// Base de datos
+var bbdd = require('./config/database.js').database;
 
 // Configuramos la aplicación
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('password'));
-  app.use(express.session({secret: 'password'}));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('password'));
+    app.use(express.session({secret: 'password'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 //conexion con twitter
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',//cambiar por el host de la base de datos
-  user     : 'administrador',//usuario de la base de datos
-  password : '123456',//password del usuario
-  database : 'mapaturistico'//nombre de la base de datos
-});
+var connection = mysql.createConnection(bbdd);
 connection.connect();
-
-
 
 
 //le pasamos el usuario y crea la sesión
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 
 passport.deserializeUser(function(id, done) {
-  connection.query('SELECT * FROM usuarios WHERE id="'+id+'"', function (err,usuario){
-              done(err, usuario);
-  });
+    connection.query('SELECT * FROM usuarios WHERE id="'+id+'"', function (err,usuario){
+        done(err, usuario);
+    });
 });
 
 passport.use(new TwitterStrategy({
     consumerKey: "sVWLivQC1afK6ULcUqWjg",//consumer key
     consumerSecret: "uGpUX34oJ8h7Hp5jiETk1hK1lgFykS6qXNM5vvf7QC0", //consumer secret
     callbackURL: "http://localhost:3000/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, done) {
+},
+function(token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
       //si es válido la cuenta de twitter comprobamos que en nuestra base de datos este usuario no lo hayamos metido
@@ -66,60 +62,60 @@ passport.use(new TwitterStrategy({
         if (err) throw err;
         if(rows.length==0)
         {
-          imagen=profile.photos[0].value;
-          connection.query('Insert into usuarios(id, usuario, imagen) values("'+id+'","'+usuario+'","'+imagen+'")', function (err,rows){
-              nuevoUsuario={"id":id, "usuario":usuario, "imagen":imagen};
-              done(null,nuevoUsuario);
+            imagen=profile.photos[0].value;
+            connection.query('Insert into usuarios(id, usuario, imagen) values("'+id+'","'+usuario+'","'+imagen+'")', function (err,rows){
+                nuevoUsuario={"id":id, "usuario":usuario, "imagen":imagen};
+                done(null,nuevoUsuario);
             });
         }else{
-          connection.query('SELECT * FROM usuarios WHERE id="'+id+'"', function (err,usuario){
-              done(err, usuario[0]);
-          });
+            connection.query('SELECT * FROM usuarios WHERE id="'+id+'"', function (err,usuario){
+                done(err, usuario[0]);
+            });
         }
       });
     });
-  }
+}
 ));
 //fin conexion con twitter
 
 // Routing
 app.get('/', function(req, res) {
-	if(typeof(req.user)=="undefined")
-	{
-		res.render('layout', {
-		title: 'Mapa en tiempo real',
-		description: 'Mi primer mapa'
-		});
-	}else{
-		res.redirect('/mapa');
-	}
-	
+    if(typeof(req.user)=="undefined")
+    {
+        res.render('layout', {
+            title: 'Mapa en tiempo real',
+            description: 'Mi primer mapa'
+        });
+    }else{
+        res.redirect('/mapa');
+    }
+    
 });
 
 app.get('/mapa', function(req, res){
-	if(typeof(req.user)=="undefined")
-	{
-		res.redirect('/');
-	}else{
-		res.render('mapa', {
-		title: 'Mapa en tiempo real',
-		description: 'Mi primer mapa',
-		usuario: req.user
-		});
-	}
+    if(typeof(req.user)=="undefined")
+    {
+        res.redirect('/');
+    }else{
+        res.render('mapa', {
+            title: 'Mapa en tiempo real',
+            description: 'Mi primer mapa',
+            usuario: req.user
+        });
+    }
 });
 
 app.get('/lugar',function(req, res){
-	res.render('place', {
-		title: 'Toronto',
-		description: 'Toronto es la ciudad canadiense más visitada por los turistas, recibiendo cada año algo más de 4 millones de personas y ocupando el puesto 14. La siguiente ciudad del país, Montreal, queda muy lejos con 679 000 turistas, según un estudio realizado con relación al año 2006',
-		autor: 'Carlos Suarez'
-	});
+    res.render('place', {
+        title: 'Toronto',
+        description: 'Toronto es la ciudad canadiense más visitada por los turistas, recibiendo cada año algo más de 4 millones de personas y ocupando el puesto 14. La siguiente ciudad del país, Montreal, queda muy lejos con 679 000 turistas, según un estudio realizado con relación al año 2006',
+        autor: 'Carlos Suarez'
+    });
 });
 
 app.get('/salir', function (req,res){
-	req.logout();
-	res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
 //rutas para login con twitter
@@ -128,15 +124,15 @@ app.get('/auth/twitter', passport.authenticate('twitter'));
 
 //url dónde devuelve si realmente fue correcto o no
 app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { successRedirect: '/mapa',
-                                     failureRedirect: '/' }));
+    passport.authenticate('twitter', { successRedirect: '/mapa',
+        failureRedirect: '/' }));
 
 
 
 io.sockets.on('connection', function (socket) {
-	socket.on('coords:me', function (data) {
-		socket.broadcast.emit('coords:user', data);
-	});
+    socket.on('coords:me', function (data) {
+        socket.broadcast.emit('coords:user', data);
+    });
 });
 
 // Iniciamos server en el puerto 3000
