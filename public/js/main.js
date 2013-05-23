4a9d36e308aaf218c8d57645abeb88cb31e0343a
Jose Alvarez <josealvarez712@gmail.com>
2013-05-23 03:01:19 -0500

    :S

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-function onDocumentReady() {
-	var socket = io.connect(window.location.href);
-
-    var map = L.map('mimapa', {
-    	center: [0, -28],
-    	zoom: 3
-    });
-
-	var tiles = L.tileLayer('http://a.tiles.mapbox.com/v3/calosth.map-v5t9aoe3/{z}/{x}/{y}.png');
-
-	map.addLayer(tiles);
-
-	map.locate({
-		enableHighAccuracy: true
-	});
-
-	map.on('locationfound', onLocationFound);
-
-	socket.on('coords:user', onReceiveData);
-
-	function onLocationFound(position) {
-		var mycoords = position.latlng;
-		var marker = L.marker([mycoords.lat, mycoords.lng]);
-
-		map.addLayer(marker);
-		marker.bindPopup('<b>Estás aquí</b>');
-
-		socket.emit('coords:me', {latlng: mycoords});
-	}
-
-	function onReceiveData(data) {
-		var coordsUser = data.latlng;
-		var marker = L.marker([coordsUser.lat, coordsUser.lng]);
-
-		map.addLayer(marker);
-		marker.bindPopup('Estás aquí');
-	}
-
-	// Efectos para mostrar/ocultar formulario.
-	var $form = $("#formulario");
-	var $place = $("#place");
-	$form.fadeIn(); // Mostrar formualrio al iniciar.
-	// Oculta el formulario.
-	$("#cerrar").on("click", function(e){
-		e.preventDefault();
-		$form.fadeOut();
-		$place.fadeOut();
-	});
-	//Efecto para que se pueda mover el formulario.
-	$form.draggable();
-}
-
-$(document).on('ready', onDocumentReady);
-
-
-
-
-=======
 function onDocumentReady() {
 	var socket = io.connect(window.location.href);
 
@@ -146,4 +87,3 @@ $(document).on('ready', onDocumentReady);
 
 
 
->>>>>>> mapa-turistico/master
