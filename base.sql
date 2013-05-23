36fce6dc04a8cac323f6991bb78dda232cf87468
Jose Alvarez <josealvarez712@gmail.com>
2013-05-23 15:27:25 -0500

    Borrar todo

--- base.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(255) NOT NULL,
-  `imagen` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF-8;
