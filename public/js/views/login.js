c32561c27b5b3cca69879d0f852ef97bf9b3c6a7
josedeweb <josealvarez712@gmail.com>
2013-05-26 13:08:25 -0500

    stylus boton de salir

--- public/js/views/login.js
+++ public/js/views/login.js
@@ -1,6 +1,7 @@
-var LoginView = Backbone.View.extend({
+var LoginView = Backbone.View.extend({	
 	el:'#logintt',
 	initialize: function(){
+		console.log("entro a esta cosa");
 		var self = this;
 		var loginButton = $('#loginButton');
 		function onLoginClick(e){
@@ -8,5 +9,10 @@ var LoginView = Backbone.View.extend({
 			self.$el.fadeToggle();
 		}
 		loginButton.on('click', onLoginClick);
+	},
+	loginSuccess: function(){
+		console.log("Entra");
+		var profile = $.get("/profile");
+		console.log(profile);
 	}
 });
