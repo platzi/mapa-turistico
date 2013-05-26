380e6c2608b58cdea39c700002a77ca16adecf57
David Inga <david.inga@icloud.com>
2013-05-26 20:28:01 +0200

    profile

--- app/controller/index.js
+++ app/controller/index.js
@@ -3,6 +3,12 @@
 module.exports = function (req, res) {
     res.render('index', {
         title: 'Mapa en tiempo real',
-        description: 'Mi primer mapa'
+        description: 'Mi primer mapa',
+        profile: {
+			"id": "51a2515fb358b1cc03000002",
+			"username": "proudplaces",
+			"provider": "twitter",
+			"avatar": "https://si0.twimg.com/profile_images/3636820877/2b7b8014b81978b5cf1ed8e16f91a15e_normal.png"
+		}
     });
 };
