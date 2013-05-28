b414cdbab03bc5d698e54314dd885deed8d6381e
David Inga <david.inga@icloud.com>
2013-05-28 09:26:18 +0200

    algunos retoques

--- public/js/views/sidebar.js
+++ public/js/views/sidebar.js
@@ -1,11 +1,12 @@
 var SidebarView = Backbone.View.extend({
 	el: '#placeList',
-
+	events: {
+		"click .elemList": "onClickSidebar"
+	},
 	addPlaces: function(places) {
 	    var htmlPlaces = "";
 
 	    _.each(places.toJSON(), function(place, index){
-
 	    	htmlPlaces += '<article data-lat="' + place.point.lat + '" data-lng="' + place.point.lng + '" class="elemList" id="' + place._id + '">';
 	    	htmlPlaces += '<figure><div class="thumb"><img src="' + place.image + '" /></div>';
 	    	htmlPlaces += '<figcaption class="caption"><p class="name">' + place.name + '</p>';
@@ -15,24 +16,13 @@ var SidebarView = Backbone.View.extend({
 
 	    this.$el.html(htmlPlaces);
 	},
-	events: {
-		"click .elemList": "onClickSidebar"
-	},
 	onClickSidebar: function(param){
-
 		var placeId = param.currentTarget.id;
 
 		var placeLat = $("#" + placeId).attr("data-lat");
 		var placeLng = $("#" + placeId).attr("data-lng");
 		var placeName = $("#" + placeId + " .name").text();
-		console.log(placeLat, placeLng, placeName);
 
 		app.map.centerMap(placeLat, placeLng, placeName);
-
-
-		//var elemento = $(this).data("place");
-		//var valor = this.$("p").attr('data-place');
-		//console.log(valor);
-
 	}
 });
