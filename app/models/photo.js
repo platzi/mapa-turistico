40892796a1c3fa2fa40a6fe0edea60c4d8a7310d
Eduardo Diaz <eduar.diaz37@gmail.com>
2013-05-31 14:44:44 -0400

    add module file-move

--- app/models/photo.js
+++ app/models/photo.js
@@ -4,21 +4,8 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema,
     ObjectId = Schema.ObjectId,
     path     = require('path'),
-    fs       = require('fs'),
-    photoSchema,
-    alternate_upload;
-
-alternate_upload = function (origin, destination, next) {
-    var read  = fs.createReadStream(origin),
-        write = fs.createWriteStream(destination);
-
-    read.pipe(write);
-    write.on('close', function () {
-        fs.unlink(origin, function (err) {
-            next(err);
-        });
-    });
-};
+    move     = require('file-move'),
+    photoSchema;
 
 var methods = {
     upload: function (path_file, next) {
@@ -26,10 +13,7 @@ var methods = {
             path_upload = path.join(__dirname, '../../uploads/',
                 (String(self._id) + '.' + self.image.ext));
 
-        fs.rename(path_file, path_upload, function (err) {
-            if (err) { return alternate_upload(path_file, path_upload, next); }
-            next(null);
-        });
+        move(path_file, path_upload, next);
     }
 };
 
